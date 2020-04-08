10d956e37bf0690ba13ecfd95e4aace5dd22e689
Armoilane <armo@iki.fi>
2020-04-08 17:14:11 +0200

    Fix empty string returning true

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = Phrase;
 
-// Adds reverse a all strings
+// Adds reverse for all strings
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
 }
@@ -9,36 +9,24 @@ String.prototype.reverse = function() {
 function Phrase(content) {
   this.content = content;
 
-  this.processor = function(string) {
-    return string.toLowerCase();
-  }
-
   // Returns content processed for palindrome testing
   this.processedContent = function processedContent() {
     return this.letters().toLowerCase();
   }
 
 // Returns the letters in the content
-//  this.letters = function letters() {
-//    let theLetters = [];
-//    Array.from(this.content).forEach( function(character) {
-//      if (character.match(letterRegex)) {
-//        theLetters.push(character);
-//      }
-//    });
-//    return theLetters.join("");
-//  }
-
-
   this.letters = function letters() {
     const lettersRegex = /[a-z]/gi;
 
-    //return Array.from(this.content).filter( c => c.match(/[a-z]/i)).join("");
     return ( this.content.match(lettersRegex) || [] ).join("");
   }
 
   // Returns true for a palindrome
   this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().reverse();
+    if ( this.processedContent() ) {
+      return this.processedContent() === this.processedContent().reverse();
+    } else {
+      return false;
+    }
   }
 }
